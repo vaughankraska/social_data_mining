6417f7b0cd0aa96402fba96c65a47c949c99d9df
vaughankraska <vaughankraska@gmail.com>
2024-11-15 18:11:25 +0100

    inserts working on tweets
    gotta figure out how to make referenced tweets performant.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import sqlite3
 from sqlite3 import Connection
 
-DB_NAME = "sdm.db"
+DB_NAME = "data/twitter.db"
 db_connection: Connection = sqlite3.connect(DB_NAME)
--- src/crud.py
+++ src/crud.py
@@ -1,4 +1,38 @@
-import json
+from sqlite3 import Connection
 from typing import Dict, Any
 
-# TODO sql commands
+
+def create_tweets_table(db: Connection):
+    cur = db.cursor()
+    query = """
+    CREATE TABLE IF NOT EXISTS tweets (
+            id TEXT PRIMARY KEY,
+            text TEXT,
+            author_id TEXT,
+            created_at TEXT,
+            possibly_sensitive BOOLEAN,
+            lang TEXT,
+            conversation_id TEXT,
+            public_metrics TEXT,
+            attachments TEXT,
+            in_reply_to_user_id TEXT,
+            geo TEXT,
+            withheld TEXT,
+            entities TEXT,
+            context_annotations TEXT
+            )
+    """
+    cur.execute(query)
+
+
+def create_ref_tweets_table(db: Connection):
+    cur = db.cursor()
+    query = """
+    CREATE TABLE IF NOT EXISTS referenced_tweets (
+            tweet_id TEXT,
+            referenced_tweet_id TEXT,
+            referenced_tweet_type TEXT,
+            FOREIGN KEY (tweet_id) REFERENCES tweets(id)
+            )
+    """
+    cur.execute(query)
