51f45948703388846cffbc9d5d6b8a5fef3ed2e5
anandmatt <anandmathewms@gmail.com>
2024-11-18 21:39:39 +0100

    added 1.4, 1.5, 1.6 funcs to utils

--- src/utils.py
+++ src/utils.py
@@ -73,3 +73,55 @@ def analyze_graph(g: ig.Graph):
     plt.grid(which="both", linestyle="--", alpha=0.7)
     plt.legend()
     plt.show()
+
+# Function for task 1.4
+def prepare_graph_for_degree(g: ig.Graph, base_size = 1, scale_factor = 5):
+    degrees = g.degree()
+    
+    g.vs["size"] = [base_size + scale_factor * degree for degree in degrees]
+    
+    return g
+
+# Function for task 1.5
+def prepare_graph_for_betweenness(g: ig.Graph, base_size = 1, scale_factor = 5):
+    betweenness = g.betweenness()
+    max_betweenness = max(betweenness) if max(betweenness) > 0 else 1
+    
+    g.vs["size"] = [base_size + scale_factor * (b/max_betweenness) for b in betweenness]
+    
+    return g
+
+# Function to plot the graph
+def plot_graph(g: ig.Graph, layout: ig.Layout, target_file = None):
+    visual_style = {
+        "layout": layout,
+        "vertex_color": g.vs["color"],
+        "vertex_size": g.vs["size"],
+        "vertex_frame_width": 0,
+        "vertex_label": None,
+        "edge_color": g.es["color"],
+        "edge_width": g.es["width"],
+        # "bbox": (800, 800),  # Image size in pixels
+        "margin": 2,
+    }
+
+    # Use matplotlib as the engine for visualization
+    plt.figure(figsize=(10, 10))
+    ig_plot = ig.plot(g, target=target_file, **visual_style, backend="matplotlib")
+
+    if target_file is None:
+        plt.show()  # Show plot interactively
+    else:
+        print(f"Plot saved to: {target_file}")
+
+# Function for task 1.6
+def top_10_for_network(g: ig.Graph):
+    degrees = g.degree()
+    top_10_degrees = sorted(degrees, reverse=True)[:10]
+    top_10_degree_authors = [g.vs[i]["name"] for i in top_10_degrees]
+    
+    betweenness = g.betweenness()
+    top_10_betweenness = sorted(betweenness, reverse=True)[:10]
+    top_10_betweenness_authors = [g.vs[i]["name"] for i in top_10_betweenness]
+    
+    return top_10_degree_authors, top_10_betweenness_authors
