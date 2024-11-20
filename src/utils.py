b445d01f7e0c782f661b8dd0cdae18de6b2b635b
vaughankraska <vaughankraska@gmail.com>
2024-11-20 17:33:51 +0100

    updated circle graphs and task 2 progress

--- src/utils.py
+++ src/utils.py
@@ -87,7 +87,7 @@ def prepare_graph(g: ig.Graph, layout: ig.Layout = None) -> Tuple[ig.Graph, ig.L
     g.vs["frame_width"] = 0
     g.vs["label"] = None
 
-    g.es["color"] = "rgba(0, 0, 0, 0.1)"
+    g.es["color"] = "rgba(1, 1, 1, 0.01)"
     g.es["width"] = 0.10
 
     return g, layout
