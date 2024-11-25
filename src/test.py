fd0357438b0f8b1dc6befb0311b1da4f059da19b
vaughankraska <vaughankraska@gmail.com>
2024-11-25 19:44:11 +0100

    bot detection done?

--- src/test.py
+++ src/test.py
@@ -1,74 +1,14 @@
 from src.config import get_db_connection
-from src.utils import common_urls_as_dataframe
+from src.utils import common_ideology
 
 
 def main(seconds_between: int):
     db = get_db_connection()
 
-    df = common_urls_as_dataframe(db=db, seconds_between=seconds_between)
+    df = common_ideology(db=db, seconds_between=seconds_between)
     print(df.head())
-    df.to_csv(f"data/df_url_connections_s{seconds_between}.csv")
-
-# def main(seconds_between: int):
-#     db = get_db_connection()
-#     cur = db.cursor()
-
-#     res = cur.execute("""
-#         WITH tweet_urls AS (
-#             SELECT
-#                 tweets.id,
-#                 tweets.text,
-#                 tweets.author_id,
-#                 json_extract(value, '$.expanded_url') AS url,
-#                 tweets.lang,
-#                 datetime(created_at) AS created_at
-#             FROM tweets,
-#                 json_each(json_extract(entities, '$.urls'))
-#             WHERE
-#                 json_extract(entities, '$.urls') IS NOT NULL
-#         )
-#         SELECT
-#             t1.id AS id1,
-#             t2.id AS id2,
-#             t1.url,
-#             t1.text AS text1,
-#             t2.text AS text2,
-#             t1.author_id AS author1,
-#             t2.author_id AS author2,
-#             t1.created_at AS time1,
-#             t2.created_at AS time2,
-#             abs(julianday(t1.created_at) - julianday(t2.created_at)) * 86400 AS seconds_diff
-#         FROM tweet_urls t1
-#         JOIN tweet_urls t2 ON
-#             t1.url = t2.url AND
-#             t1.id != t2.id AND
-#             t1.author_id != t2.author_id
-#         WHERE
-#             seconds_diff <= ?
-#     """, [seconds_between])
-#     rows = res.fetchall()
-#     print(rows)
-#     print(len(rows))
-
-
-# def main(seconds_between: int):
-#     db = get_db_connection()
-#     cur = db.cursor()
-
-#     res = cur.execute("""
-#             SELECT
-#                 tweets.id,
-#                 tweets.author_id,
-#                 json_extract(value, '$.expanded_url') AS url,
-#                 tweets.lang,
-#                 datetime(created_at) AS created_at
-#             FROM tweets,
-#                 json_each(json_extract(entities, '$.urls'))
-#             WHERE
-#                 json_extract(entities, '$.urls') IS NOT NULL
-#                 """)
-#     print(len(res.fetchall()))
+    df.to_csv(f"data/df_ideology_connections_s{seconds_between}.csv")
 
 
 if __name__ == "__main__":
-    main(1)
+    main(60)
--- src/utils.py
+++ src/utils.py
@@ -32,6 +32,67 @@ def tweets_as_dataframe(file_path: str = "data/tweets.dat", save=False):
         print("Error in tweets_as_dataframe: ", e)
 
 
+def common_ideology(db: Connection, seconds_between: int, save_csv: bool = False):
+    cur = db.cursor()
+
+    cur.execute("""
+        CREATE TEMP TABLE tweet_urls AS
+        SELECT
+            tweets.id,
+            tweets.text,
+            tweets.author_id,
+            json_extract(value, '$.expanded_url') AS url,
+            tweets.lang,
+            tweets.possibly_sensitive,
+            datetime(created_at) AS created_at
+        FROM tweets,
+            json_each(json_extract(entities, '$.urls'))
+        WHERE
+            json_extract(entities, '$.urls') IS NOT NULL
+        AND text NOT LIKE 'RT @%'
+    """)
+
+    # Create indexes for speed
+    cur.execute("CREATE INDEX temp.idx_tweet_urls_url ON tweet_urls(url)")
+    cur.execute("CREATE INDEX temp.idx_tweet_langs ON tweet_urls(lang)")
+    cur.execute("CREATE INDEX temp.idx_tweet_urls_id ON tweet_urls(id)")
+    cur.execute("CREATE INDEX temp.idx_tweet_urls_author ON tweet_urls(author_id)")
+    cur.execute("CREATE INDEX temp.idx_tweet_urls_combined ON tweet_urls(url, id, author_id)")
+
+    res = cur.execute("""
+        SELECT
+            t1.id AS id1,
+            t2.id AS id2,
+            t1.url,
+            t1.text AS text1,
+            t2.text AS text2,
+            t1.lang as lang1,
+            t2.lang as lang2,
+            t1.author_id AS author1,
+            t2.author_id AS author2,
+            t1.created_at AS time1,
+            t2.created_at AS time2,
+            abs(julianday(t1.created_at) - julianday(t2.created_at)) * 86400 AS seconds_diff
+        FROM tweet_urls t1
+        JOIN tweet_urls t2 ON
+            t1.url = t2.url AND
+            t1.id < t2.id AND
+            t1.author_id < t2.author_id AND
+            t1.lang = t2.lang AND
+            t1.possibly_sensitive = t2.possibly_sensitive
+        WHERE
+            abs(julianday(t1.created_at) - julianday(t2.created_at)) * 86400 <= ?
+    """, [seconds_between])
+    rows = res.fetchall()
+    cur.execute("DROP TABLE tweet_urls")
+    print(f"Found {rows} entries")
+    df = pd.DataFrame(rows)
+    if save_csv:
+        df.to_csv(f"df_url_connections_s{seconds_between}.csv")
+
+    return df
+
+
 def common_urls_as_dataframe(db: Connection, seconds_between: int, save_csv: bool = False):
     cur = db.cursor()
 
