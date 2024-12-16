b7a153dc8a82033a590a4921a026d7a194bef736
todomoldovan <tmoldovan@conncoll.edu>
2024-12-16 10:35:04 +0100

    create tables

---
+++ example_data/make_tables.sql
@@ -0,0 +1,55 @@
+-- Create table test_redditor
+CREATE TABLE redditor (
+    redditor_id varchar primary key,
+    name varchar,
+    created_at timestamptz,
+    karma jsonb,
+    is_gold boolean,
+    is_mod jsonb,
+    trophy jsonb,
+    removed varchar
+);
+
+-- Enable row-level security on test_redditor
+-- ALTER TABLE test_redditor ENABLE ROW LEVEL SECURITY;
+
+-- Create table test_submission
+CREATE TABLE submission (
+    submission_id varchar primary key,
+    redditor_id varchar,
+    created_at timestamptz,
+    title varchar,
+    text text,
+    subreddit varchar,
+    permalink varchar,
+    attachment jsonb,
+    flair jsonb,
+    awards jsonb,
+    score jsonb,
+    upvote_ratio jsonb,
+    num_comments jsonb,
+    edited boolean,
+    archived boolean,
+    removed boolean,
+    poll jsonb
+); 
+
+-- Enable row-level security on test_submission
+-- ALTER TABLE test_submission ENABLE ROW LEVEL SECURITY;
+
+-- Create table test_comment
+CREATE TABLE comment(
+    comment_id varchar primary key,
+    link_id varchar,
+    subreddit varchar, 
+    parent_id varchar,
+    redditor_id varchar,
+    created_at timestamptz,
+    body text,
+    score jsonb,
+    edited boolean,
+    removed varchar
+); 
+
+-- Enable row-level security on test_comment
+-- ALTER TABLE test_comment ENABLE ROW LEVEL SECURITY;
