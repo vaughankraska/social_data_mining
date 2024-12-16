f21a2cb10cb64857c37ee9d2db6ab9c1c075d02f
vaughankraska <vaughankraska@gmail.com>
2024-12-16 09:43:41 -0700

    refactor get_db_connection
    get_db_connection now takes `db_type` and I have refactored all of our
    ipynb files in order to use sqlite and be backwards compatible.

--- src/sdm/__init__.py
+++ src/sdm/__init__.py
@@ -1,22 +1,20 @@
 from sdm.config import get_db_connection
 from sdm.crud import ingest_accounts
-from text_mining.data import get_tweet_corpora
 
 
 def insert_accounts() -> None:
     print("Ingesting accounts")
-    db = get_db_connection()
-    ingest_accounts(db)
-    res = db.cursor().execute("SELECT COUNT(*) from accounts").fetchall()
-    print(f"Accounts count: {res}")
+    with get_db_connection(db_type="sqlite") as db:
+        ingest_accounts(db)
+        res = db.cursor().execute("SELECT COUNT(*) from accounts").fetchall()
+        print(f"Accounts count: {res}")
 
 
 def test() -> None:
-    db = get_db_connection()
-    min_chars = 100
-    df = get_tweet_corpora(db, min_chars=min_chars)
-    print(df.info())
-    print(df.head())
+    with get_db_connection(db_type="postgres") as db:
+        with db.cursor() as cur:
+            cur.execute("SELECT version()")
+            print(cur.fetchone())
 
 
 def main() -> None:
--- src/sdm/config.py
+++ src/sdm/config.py
@@ -1,7 +1,17 @@
+from typing import Union, Literal
 import sqlite3
-from sqlite3 import Connection, Cursor
+from sqlite3 import Connection as LiteConnection, Cursor
+import psycopg
+from psycopg import Connection as PostGresConnection
+
+Connection = Union[LiteConnection, PostGresConnection]
 
 DB_NAME = "data/twitter.db"
+PG_NAME = "sdm"
+PG_PASSWORD = "postgres"
+PG_USER = "postgres"
+PG_HOST = "localhost"
+PG_PORT = 5432
 
 
 def dict_factory(cursor: Cursor, row):
@@ -9,9 +19,49 @@ def dict_factory(cursor: Cursor, row):
     return {key: value for key, value in zip(fields, row)}
 
 
-def get_db_connection(db_path: str = DB_NAME, use_dict_reponses=True):
+def get_db_connection(db_type: Literal["sqlite", "postgres"], **kwargs) -> Connection:
+    """
+    Get db connection to either 'slqite' or 'postgres'. Both use the DB-API
+    protocol so it should work across the same queries (fingers crossed)
+    Args:
+        db_type (str): Type of database ('sqlite' or 'postgres').
+        **kwargs: Database-specific connection parameters eg
+        'db_path' and 'use_dict_responses' for sqlite.
+    """
+
+    if db_type == "sqlite":
+        return get_lite_connection(
+                db_path=kwargs.get("db_path", DB_NAME),
+                use_dict_responses=kwargs.get("use_dict_responses", True)
+                )
+    elif db_type == "postgres":
+        return get_pg_connection()
+    else:
+        raise ValueError(f"Unknown db_type '{db_type}'.")
+
+
+def get_lite_connection(db_path: str, use_dict_responses: bool) -> LiteConnection:
     db_connection: Connection = sqlite3.connect(db_path)
-    if use_dict_reponses:
+    if use_dict_responses:
         db_connection.row_factory = dict_factory
 
     return db_connection
+
+
+def get_pg_connection() -> PostGresConnection:
+    """
+    Returns a PostgreSQL connection using **psycopg3**.
+
+    Usage:
+        with get_pg_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT version();")
+            print(cur.fetchone())
+    """
+    return psycopg.connect(
+        dbname=PG_NAME,
+        user=PG_USER,
+        password=PG_PASSWORD,
+        host=PG_HOST,
+        port=PG_PORT
+    )
