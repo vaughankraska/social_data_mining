a8aad1d4014d1d336cc8f02028f58860aff8a875
anandmatt <anandmathewms@gmail.com>
2025-01-04 19:35:26 +0100

    Setup for image analysis lab

---
+++ packages/image-analysis/src/image_analysis/data.py
@@ -0,0 +1,24 @@
+from sqlite3 import Connection
+
+import pandas as pd
+
+def get_images_dataframe(db: Connection) -> pd.DataFrame:
+    cur = db.cursor()
+    res = cur.execute("""
+        SELECT
+            a.author_id AS account_id,
+            t.attachments AS image_id,
+            a.account_type,
+            a.lang,
+            a.stance
+        FROM
+            accounts a
+        JOIN
+            tweets t
+        ON
+            a.author_id = t.author_id
+        WHERE
+            a.account_type IN ('Private individuals', 'Business actors')                  
+    """).fetchall()   # Since it's images, have removed the english filter a.lang = 'en'
+    
+    return pd.DataFrame(res)
